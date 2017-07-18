cb6b1fa053b4e7077d64ab7def51517ab9f6f7dc
Matt Meng <ymeng.ucla@gmail.com>
2017-07-17 18:48:11 -0700

    update the parallel model training

--- runner_in_parallel.py
+++ runner_in_parallel.py
@@ -1,33 +1,23 @@
-import os, subprocess, stat
-from hybrid_model import HybridModel, create_local_log_path
+import subprocess
+from hybrid_model import HybridModel, create_local_log_path, generate_multi_model_tensorboard_script
 
 common_command_line = ["python", "runner.py"]
-learning_rate_list = [0.00001]
-batch_size_list = [1, 10, 50, 100]
+learning_rate_list = [0.00001, 0.0001, 0.001, 0.01]
+batch_size_list = [100]
 job_command_list = []
 model_name_list = []
-
-
-def generate_multi_model_tensorboard_script(log_path_dict):
-    file_name = "start_multi_model_tensorboard.sh"
-    logdir = ",".join(["{}:{}".format(name, model_log_path) for name, model_log_path in log_path_dict.iteritems()])
-    with open(file_name, "w") as text_file:
-        text_file.write("#!/bin/bash \n")
-        text_file.write("tensorboard --logdir={}".format(logdir))
-    st = os.stat(file_name)
-    os.chmod(file_name, st.st_mode | stat.S_IEXEC)
 
 
 def run_jobs_in_parallel(job_commands, model_name_list):
     processes = set()
     log_path_dict = {}
-    for command_lines, model_name in zip(job_commands, model_name_list):
+    for command_line, model_name in zip(job_commands, model_name_list):
         local_log_path = create_local_log_path(HybridModel.COMMON_PATH, model_name)
         log_path_dict[model_name] = local_log_path
         log_file = "{}_log.txt".format(model_name)
-        print "running process:", " ".join(command_lines)
+        print "running process:", " ".join(command_line)
         with open(log_file, 'w') as output:
-            processes.add(subprocess.Popen(command_lines, stdout=output, bufsize=0))
+            processes.add(subprocess.Popen(command_line, stdout=output, bufsize=0))
 
     generate_multi_model_tensorboard_script(log_path_dict)
     for p in processes:
@@ -37,7 +27,7 @@ def run_jobs_in_parallel(job_commands, model_name_list):
 
 for learning_rate in learning_rate_list:
     for batch_size in batch_size_list:
-        model_name = "NYDN_hybrid_model_learning_rate_{}_batch_size_{}".format(learning_rate, batch_size)
+        model_name = "model_learning_rate_{}_batch_size_{}".format(learning_rate, batch_size)
         args = {}
         args['--model_name'] = model_name
         args['--learning_rate'] = str(learning_rate)
