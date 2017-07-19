ade418ca8f25ed06032221e02e9a084de64fd7b0
Matt Meng <ymeng.ucla@gmail.com>
2017-07-19 16:02:14 -0700

    create the entry point for model training with index

--- runner_in_parallel.py
+++ runner_in_parallel.py
@@ -1,5 +1,4 @@
-import subprocess
-from hybrid_model import HybridModel, create_local_log_path, generate_multi_model_tensorboard_script
+from run_utils import run_jobs_in_parallel
 
 common_command_line = ["python", "runner.py"]
 learning_rate_list = [0.005, 0.001, 0.0005]
@@ -7,26 +6,8 @@ batch_size_list = [10, 50, 100, 200, 500, 1000]
 job_command_list = []
 model_name_list = []
 
-
-def run_jobs_in_parallel(job_commands, model_name_list):
-    processes = set()
-    log_path_dict = {}
-    for command_line, model_name in zip(job_commands, model_name_list):
-        local_log_path = create_local_log_path(HybridModel.COMMON_PATH, model_name)
-        log_path_dict[model_name] = local_log_path
-        log_file = "{}_log.txt".format(model_name)
-        print "running process:", " ".join(command_line)
-        with open(log_file, 'w') as output:
-            processes.add(subprocess.Popen(command_line, stdout=output, bufsize=0))
-
-    generate_multi_model_tensorboard_script(log_path_dict)
-    for p in processes:
-        if p.poll() is None:
-            p.wait()
-
-
-for learning_rate in learning_rate_list:
-    for batch_size in batch_size_list:
+for batch_size in batch_size_list:
+    for learning_rate in learning_rate_list:
         model_name = "NYDN_hybrid_model_learning_rate_{}_batch_size_{}".format(learning_rate, batch_size)
         args = {}
         args['--model_name'] = model_name
---
+++ single_runner_by_index.py
@@ -0,0 +1,53 @@
+import os, argparse
+from series_data_generator import SeriesDataGenerator
+from google_cloud_storage_util import GCS_Bucket
+from hybrid_model import HybridModel
+from utils import GCS_BUCKET_NAME
+from data_preprocess import load_training_data_from_gcs, load_yaml_file_from_gcs, create_train_test_by_index
+
+
+def main():
+    '''entry point for TensorFlow model training, this model training
+    requires a yaml file in GCS to provide the set of `train_index` and
+    `test_index` used in function `create_train_test_by_index`.
+
+    command-line entry points
+        1. --learning_rate: the learning rate used in model training
+        2. --batch_size: the batch size used in model training
+        3. --model_name: the model name used in model training
+
+        4. --gcs_path: GCS path for the model configuration yaml file
+        5. --yaml_file_name: yaml configuration file name in GCS
+        6. --index_gcs_path: GCS path for the data index yaml file
+        7. --index_file_name: index yaml file name in GCS
+    '''
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-l", "--learning_rate", help="model training learning rate", type=float, default=0.00001)
+    parser.add_argument("-b", "--batch_size", help="model training batch size", type=int, default=50)
+    parser.add_argument("-n", "--model_name", help="model name, also the folder name", type=str, default="NYDN_hybrid_model")
+    parser.add_argument("--gcs_path", help="the GCS path for config_dict and data", type=str, default="test/ML")
+    parser.add_argument("--yaml_file_name", help="model name, also the folder name", type=str, default="training_configuration.yaml")
+    parser.add_argument("--index_gcs_path", help="the GCS path for config_dict and data", type=str, default="test/ML/index_yaml")
+    parser.add_argument("--index_file_name", help="model name, also the folder name", type=str)
+
+    args = parser.parse_args()
+    config_dict, local_data_file = load_training_data_from_gcs(args.gcs_path, args.yaml_file_name)
+    bucket = GCS_Bucket(GCS_BUCKET_NAME)
+    index_dict = load_yaml_file_from_gcs(bucket, args.index_gcs_path, args.index_file_name)
+
+    train, valid_data = create_train_test_by_index(local_data_file, config_dict, index_dict)
+    data_generator = SeriesDataGenerator(train, config_dict)
+    test_generator = SeriesDataGenerator(valid_data, config_dict)
+    try:
+        #tf.reset_default_graph()
+        print "learning rate: {}, batch size: {}".format(args.learning_rate, args.batch_size)
+        model = HybridModel(config_dict, args.model_name, learning_rate=args.learning_rate, batch_size=args.batch_size)
+        model.train(data_generator, test_generator)
+    except Exception, e:
+        print "found the exception {} in model training.".format(e)
+    finally:
+        os.unlink(local_data_file)
+
+if __name__ == "__main__":
+    main()
