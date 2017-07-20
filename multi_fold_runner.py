24e5ed41ff2ede0c92719036c5a4da78b52b79d6
Matt Meng <ymeng.ucla@gmail.com>
2017-07-19 18:20:59 -0700

    merge kfold with mulit-process

---
+++ multi_fold_runner.py
@@ -0,0 +1,41 @@
+import argparse
+from data_preprocess import create_kfold_data_index_yaml_files
+from run_utils import run_jobs_in_parallel
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-l", "--learning_rate", help="model training learning rate", type=float, default=0.00001)
+    parser.add_argument("-b", "--batch_size", help="model training batch size", type=int, default=50)
+    parser.add_argument("-n", "--model_name", help="model name, also the folder name", type=str, default="NYDN_hybrid_model")
+    parser.add_argument("--gcs_path", help="the GCS path for config_dict and data", type=str, default="test/ML")
+    parser.add_argument("--yaml_file_name", help="model name, also the folder name", type=str, default="training_configuration.yaml")
+    parser.add_argument("--index_gcs_path", help="the GCS path for config_dict and data", type=str, default="test/ML/index_yaml")
+    parser.add_argument("--fold_num", help="fold number for cross-validation", type=int, default=3)
+    args = parser.parse_args()
+    yaml_file_list, model_name_list = create_kfold_data_index_yaml_files(args.gcs_path,
+                                                                         args.yaml_file_name,
+                                                                         args.index_gcs_path,
+                                                                         args.model_name,
+                                                                         args.fold_num)
+    common_command_line = ["python", "single_runner_by_index.py"]
+    job_command_list = []
+    for yaml_file, model_name in zip(yaml_file_list, model_name_list):
+        args_dict = {}
+        args_dict['--learning_rate'] = str(args.learning_rate)
+        args_dict['--batch_size'] = str(args.batch_size)
+        args_dict['--gcs_path'] = args.gcs_path
+        args_dict['--index_gcs_path'] = args.index_gcs_path
+        args_dict['--model_name'] = model_name
+        args_dict['--index_file_name'] = yaml_file
+        command_lines = common_command_line[:]
+        command_lines.extend(reduce(lambda x, y: x + y, args_dict.items()))
+        print command_lines
+        model_name_list.append(model_name)
+        job_command_list.append(command_lines)
+
+    print "start running {} jobs".format(len(job_command_list))
+    run_jobs_in_parallel(job_command_list, model_name_list)
+
+if __name__ == '__main__':
+    main()
--- runner_in_parallel.py
+++ runner_in_parallel.py
@@ -1,18 +1,28 @@
 from run_utils import run_jobs_in_parallel
 
-common_command_line = ["python", "runner.py"]
+#common_command_line = ["python", "runner.py"]
+common_command_line = ["python", "multi_fold_runner.py"]
 learning_rate_list = [0.005, 0.001, 0.0005]
-batch_size_list = [10, 50, 100, 200, 500, 1000]
+batch_size_list = [500]
 job_command_list = []
 model_name_list = []
+GCS_path = 'test/ML'
+yaml_GCS_path = 'test/ML/index_yaml'
+yaml_file_name = 'training_configuration.yaml'
 
 for batch_size in batch_size_list:
     for learning_rate in learning_rate_list:
-        model_name = "NYDN_hybrid_model_learning_rate_{}_batch_size_{}".format(learning_rate, batch_size)
+        model_name = "learning_rate_{}_batch_size_{}".format(learning_rate, batch_size)
         args = {}
         args['--model_name'] = model_name
         args['--learning_rate'] = str(learning_rate)
         args['--batch_size'] = str(batch_size)
+
+        args['--gcs_path'] = GCS_path
+        args['--yaml_file_name'] = yaml_file_name
+        args['--index_gcs_path'] = yaml_GCS_path
+        args['--fold_num'] = str(4)
+
         command_lines = common_command_line[:]
         command_lines.extend(reduce(lambda x, y: x + y, args.items()))
         model_name_list.append(model_name)
