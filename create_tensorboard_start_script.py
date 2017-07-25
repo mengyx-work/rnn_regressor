eb51590762eaa5b7935f30df5370ae3969cd7c12
Matt Meng <ymeng.ucla@gmail.com>
2017-07-24 21:52:04 -0700

    update script for tensorboard

--- create_tensorboard_start_script.py
+++ create_tensorboard_start_script.py
@@ -1,11 +1,19 @@
-import sys, yaml
+import os, sys, yaml, argparse
 from hybrid_model import generate_multi_model_tensorboard_script
 
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--all', help='use all the yaml files', action='store_true')
+    args = parser.parse_args()
+    if args.all:
+        mylist = os.listdir(os.getcwd())
+        file_list = [file_name for file_name in mylist if ".yaml" in file_name]
+    else:
+        file_list = sys.argv[1:]
     model_dict = {}
-    for file_name in sys.argv[1:]:
-	print "file name: {}".format(file_name)
+    for file_name in file_list:
+        print "file name: {}".format(file_name)
         with open(file_name, 'r') as yaml_file:
             single_model_dict = yaml.load(yaml_file)
         model_dict.update(single_model_dict)
