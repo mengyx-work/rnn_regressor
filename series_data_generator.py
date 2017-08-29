13afd2170643a966dc8fc9cb4fb2b8e3bff06f60
Matt Meng <ymeng.ucla@gmail.com>
2017-08-28 17:18:59 -0700

    roll back the old data generator

--- series_data_generator.py
+++ series_data_generator.py
@@ -24,49 +24,6 @@ class SeriesDataGenerator(object):
         self.index_counter = -1
         self.total_row_counts = self.get_total_counts()
         self.time_series_column_names = self._build_time_series_column_names()
-        self._cur_index = 0
-        self._generate_data_sequence(data)
-
-    @staticmethod
-    def _collect_data_by_index(data_sequence, start_index, end_index, time_series_data=[], meta_data=[], target=[]):
-        for i in xrange(start_index, end_index):
-            time_series_data.append(data_sequence[i]['time_series_data'])
-            meta_data.append(data_sequence[i]['meta_data'])
-            target.append(data_sequence[i]['target'])
-        return time_series_data, meta_data, target
-
-    @staticmethod
-    def _extract_single_instance(data, time_series_column_names, config_dict, index):
-        time_series_data = []
-        for column_name_set in time_series_column_names:
-            time_series_data.append(data.iloc[index][column_name_set].tolist())
-        meta_data = data.iloc[index][config_dict["static_columns"]].tolist()
-        target = [data.iloc[index][config_dict["label_column"]]]
-        return {'time_series_data': time_series_data, 'meta_data': meta_data, 'target': target}
-
-    def next_batch(self, batch_size):
-        if batch_size >= 2 * self.total_row_counts:
-            raise ValueError("the batch_size can not be more than two times the total_row_counts")
-        if self._cur_index + batch_size <= self.total_row_counts:
-            start_index = self._cur_index
-            self._cur_index += batch_size
-            time_series_data, meta_data, target = self._collect_data_by_index(self.data_sequence, start_index, self._cur_index)
-        else:
-            start_index = self._cur_index
-            self._cur_index = self._cur_index + batch_size - self.total_row_counts
-            time_series_data, meta_data, target = self._collect_data_by_index(self.data_sequence, start_index, self.total_row_counts)
-            time_series_data, meta_data, target = self._collect_data_by_index(self.data_sequence, 0, self._cur_index, time_series_data, meta_data, target)
-            #batch_content = self.titles[start_index: self.total_row_counts]
-            #batch_content.extend(self.titles[0: self._cur_index])
-        return train_data(time_series_data=time_series_data, meta_data=meta_data, target=target)
-
-    def _generate_data_sequence(self, data):
-        #data = data.sample(frac=1)
-        self.data_sequence = []
-        #for index in data.index:
-        for locale_index in xrange(self.total_row_counts):
-            instance = self._extract_single_instance(data, self.time_series_column_names, self.config_dict, locale_index)
-            self.data_sequence.append(instance)
 
     def get_total_counts(self):
         return self.data.shape[0]
@@ -84,7 +41,7 @@ class SeriesDataGenerator(object):
         self.index_counter += 1
         if self.index_counter >= self.total_row_counts:
             self.index_counter = self.index_counter - self.total_row_counts
-            return self.index_counter 
+            return self.index_counter
         else:
             return self.index_counter
 
@@ -96,14 +53,13 @@ class SeriesDataGenerator(object):
 
     def _extract_time_series_data(self, index_list):
         data = []
-        for index in index_list: 
+        for index in index_list:
             instance = []
             for column_name_set in self.time_series_column_names:
                 instance.append(self.data.iloc[index][column_name_set].tolist())
             data.append(instance)
         return data
 
-    '''            
     def next_batch(self, batch_size):
         index_list = self._batch_index(batch_size)
         # build the time_series variables
@@ -114,4 +70,3 @@ class SeriesDataGenerator(object):
             meta_data.append(self.data.iloc[cur_index][self.config_dict["static_columns"]].tolist())
             target.append([self.data.iloc[cur_index][self.config_dict["label_column"]]])
         return train_data(time_series_data=time_series_data, meta_data=meta_data, target=target)
-    '''
